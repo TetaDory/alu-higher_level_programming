a3e15d628ec2abf7f5e4d465b07813be64af49e7
TetaDory <d.rutayisir1@alustudent.com>
2022-10-30 17:52:43 -0700

    Task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 
-if __name__ == '__main__':
-    args_number = len(argv) - 1
-
-    if args_number > 0:
-        sum_ = 0
-        for args in argv[1:]:
-            sum_ += int(args)
-        print("{}".format(sum_))
-     else:
-        print("{}".format(args_number))
+if __name__ == "__main__":
+    length = len(argv)
+    sum = 0
+    for i in range(1, length):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
