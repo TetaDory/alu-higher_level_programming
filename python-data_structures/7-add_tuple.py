1db02e21b085fe5ce10c3d624e4fe5951deb8e81
TetaDory <d.rutayisir1@alustudent.com>
2022-11-04 08:07:25 -0700

    Task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    leng_a, leng_b = len(tuple_a), len(tuple_b)
-    if leng_a == 0:
-        a = (0, 0)
-    elif leng_a == 1:
-        a = (tuple_a[0], 0)
-    else:
-        a = tuple_a
-    if leng_b == 0:
-        b = (0, 0)
-    elif leng_b == 1:
-        b = (tuple_b[0], 0)
-    else:                                                              b = tuple_b
-    return (a[0] + b[0], a[1] + b[1])
+    if len(tuple_a) == 0:
+        tuple_a += (0, 0)
+    elif len(tuple_a) == 1:
+        tuple_a += (0,)
+    elif len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+    if len(tuple_b) == 0:
+        tuple_b += (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_b += (0,)
+    elif len(tuple_b) > 2:
+        tuple_b = tuple_b[:2]
+    first_element = tuple_a[0] + tuple_b[0]
+    second_element = tuple_a[1] + tuple_b[1]
+    return tuple((first_element, second_element))
