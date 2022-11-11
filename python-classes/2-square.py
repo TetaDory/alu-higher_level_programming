6a6169b1c68bd324ac8cad221d15fc10e102b37a
TetaDory <d.rutayisir1@alustudent.com>
2022-11-11 13:59:27 -0800

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""A class that defines a square"""
+"""Create a square"""
 
 
 class Square:
-    """Creates a square.
-    Private instance attributes: size
+    """
+    Creates a square
+    Has a private Instance att: size
     """
 
     def __init__(self, size=0):
-        """Initializes data."""
+        """init size"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
+    
     self.__size = size
