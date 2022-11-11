c8e4043f0165bf05ae9ce1b2fa7e1d702acfe0f5
TetaDory <d.rutayisir1@alustudent.com>
2022-11-11 13:54:49 -0800

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Create a square """
+"""
+A class that defines a square by: (based on 2-square.py)
+"""
 
 
 class Square:
-    '''
-    Create a square
-        Has a private Instance att: size
-    '''
+    """Creates a square.
+    Private instance attributes: size
+    """
 
     def __init__(self, size=0):
-        ''' init size '''
+        """Initializes the data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
-                                                                  def area(self):
-        square_area = self.__size ** 2
-        return square_area
+                                                                   def area(self):
+        """Return square area."""
+        return self.__size ** 2
