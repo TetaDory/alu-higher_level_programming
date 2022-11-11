06d1a08c786ebecff21cbf9acd5c29ea20fef1e9
TetaDory <d.rutayisir1@alustudent.com>
2022-11-11 14:29:34 -0800

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python3
-"""Create a square"""
+#1/usr/bin/python3
+"""Create a square """
 
 
 class Square:
-    '''Create a square
-           Has a private Instance att: size
+    '''
+    Create a square
+        Has a private Instance att: size
     '''
 
-    def __init__(self, size=0):
-        ''' init size '''
+    def __inti__(self,size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
-        
+            raise ValueError("size must be >=0")
+
         self.__size = size
-                                                                   def area(self):
-        square_area = self.__size ** 2
+    def area(self):
+        square_are + self.__size ** 2
         return square_area
