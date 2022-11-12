22d33efb454303e9667eff9790e742ba156549d6
TetaDory <d.rutayisir1@alustudent.com>
2022-11-12 04:48:40 -0800

    Task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
         Has a private Instance att: size
     '''
 
-    def __int__(self, size=0)
+    def __init__(self, size=0):
         ''' init size '''
         self.__size = size
 
@@ -17,13 +17,15 @@ class Square:
         "returns the size att"
         return self.__size
 
-    @size.settter
+    @size.setter
     def size(self, size):
         '''asign the size to the size att'''
         if not isinstance(size, int):
-            raise TypeError("size must be >= 0")
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
         square_area = self.__size ** 2
-        return sqaure_area
+        return square_area
