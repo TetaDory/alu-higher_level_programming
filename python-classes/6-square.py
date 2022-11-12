fc609ae076b40b53867a7fc2f303d3384419bc4d
TetaDory <d.rutayisir1@alustudent.com>
2022-11-12 06:36:11 -0800

    Task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Define a square"""
+'''Creating a square'''
 
 
 class Square:
     """
-        square with private instance attribute: 'size'
+    Creates a square.
     """
 
     def __init__(self, size=0, position=(0, 0)):
