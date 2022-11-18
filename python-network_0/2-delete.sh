6563537165172f9b4c1ff20e9a2791d3bb0f2135
TetaDory <d.rutayisir1@alustudent.com>
2022-11-18 09:33:39 -0800

    D

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a 'DELETE' request to the URL passed as the first argument and displays the body of the response
+# send a delete request to a url 
 curl -sX DELETE "$1"
