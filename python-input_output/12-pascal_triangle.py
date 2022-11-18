12f7ec9d4c9bb98c40099a8b1963bef6a4780dff
TetaDory <d.rutayisir1@alustudent.com>
2022-11-18 08:11:38 -0800

    v

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-# function def pascal_triangle(n): that returns,
-# a list of lists of integers representing the,
-# Pascal’s triangle of n:
-"""
-   define a function 'pascal_triangle'
-"""
+'''Pascal’s triangle of n:'''
 
 
 def pascal_triangle(n):
-    """
-    return the pascal triangle
-    """
-    
-    if n <= 0:
-        return []
-    p_t = [[] for x in range(n)]
-    p_t[0] = [1]
-    if n > 1:
-        p_t[1] = [1, 1]
-    if n > 2:
-        for i in range(2, n):
-            p_t[i].append(1)
-            for j in range(i - 1):
-                p_t[i].append(p_t[i - 1][j] + p_t[i - 1][j + 1])
-                p_t[i].append(1)
-    return p_t
+    '''returns the pascals triangle '''
+    outer_list = []
+
+    for i in range(n):
+        if i == 0:
+            outer_list.append([1])
+        elif i == 1:
+            outer_list.append([1, 1])
+        else:
+            last_list = outer_list[-1]
+            count = 0
+            new_list = [1]
+            while count < len(last_list):
+                try:
+                    new_list.append(last_list[count] + last_list[count + 1])
+                    count += 1
+                except IndexError:
+                    break
+                new_list.append(1)
+                outer_list.append(new_list)
+    return outer_list
