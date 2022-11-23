af8e4c652d637eae893fe43bcd204daaef8870b2
TetaDory <d.rutayisir1@alustudent.com>
2022-11-23 03:54:46 -0800

    3

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -12,7 +12,7 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(request) as response:
