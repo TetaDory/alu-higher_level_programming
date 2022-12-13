9b11739a1710d8b08f56d91311e8a93a0936086c
TetaDory <113389069+TetaDory@users.noreply.github.com>
2022-12-13 22:21:12 +0200

    Create model_city.py

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Defining the class city with properties from Base"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
