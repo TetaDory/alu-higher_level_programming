d2b20ef5c1c0dbd5ad2237427ea9c3232e7a2f7c
TetaDory <d.rutayisir1@alustudent.com>
2022-12-13 11:52:05 -0800

    l

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db_conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],)
 
     cur = db_conn.cursor()
-    
+
     cur.execute(
         "SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
