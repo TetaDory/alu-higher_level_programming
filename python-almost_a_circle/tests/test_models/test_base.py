2ad0203ea505afee8cea267175fc2bd97babaebe
TetaDory <113389069+TetaDory@users.noreply.github.com>
2022-12-13 21:02:45 +0200

    Update test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""Unit testing for class base"""
+"""Unit testing for the base class"""
 
 
 import os
-from test import TestCase
+from unittest import TestCase
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
 
 class TestBase(TestCase):
-    """Defining a class that tests class Base"""
+    """The Test class for the Base class in models"""
 
     def test_ba(self):
-        """Defining a function that tests the starting point of creation of Base"""       base = Base()
+        """Test the starting point of creation of Base"""
+        base = Base()
         base_1 = Base()
         base_89 = Base(89)
         self.assertEqual(base.id, 1)
@@ -21,14 +22,14 @@ class TestBase(TestCase):
         self.assertEqual(base_89.id, 89)
 
     def test_to_json_string(self):
-        """Defining a function that tests the converting of lists to dicts"""
+        """Test the converting of lists to dicts"""
         self.assertEqual(Base.to_json_string(None), "[]")
         self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
         self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
         self.assertEqual(Base.to_json_string([]), "[]")
 
     def test_save_to_file(self):
-        """Defining a function that test that the file saves list objects to  file"""
+        """Test that the file saves list objects to  file"""
         Base._Base__nb_objects = 0
         Square.save_to_file(None)
 
@@ -65,7 +66,7 @@ class TestBase(TestCase):
                              '"height": 3, "x": 0, "y": 0}]')
 
     def test_from_json_string(self):
-        """Defining a function that tests the JSON string representation"""
+        """Test """
         self.assertEqual(Base.from_json_string(None), [])
         self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
         self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
