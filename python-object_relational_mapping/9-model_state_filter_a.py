731870f0dda9d0ab3f8431e8b39b01b92b93b93c
TetaDory <113389069+TetaDory@users.noreply.github.com>
2022-12-13 22:52:52 +0200

    Update 9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa"""
+"""Write a script that lists all State objects that contain
+the letter a from the database hbtn_0e_6_usa"""
 
 
 import sys
