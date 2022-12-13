81d01b60e9a1b2e4a8c4ff8acc117d4d5b72fad8
TetaDory <113389069+TetaDory@users.noreply.github.com>
2022-12-13 22:54:26 +0200

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that prints the first State object from the database hbtn_0e_6_usa"""
+"""Write a script that prints the first State object
+from the database hbtn_0e_6_usa"""
 
 
 import sys
